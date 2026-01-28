8334705c72639ffda7b1925c1803a77290058d6a
SDIS 66 <sdis66@sdis66.fr>
2026-01-29 00:36:13 +0100

    v1.22: Update version number in Code.js

--- Code.js
+++ Code.js
@@ -1,7 +1,7 @@
 /****************************************************
  * SDIS 66 - SDS | WebApp Dashboard
  * CACHE SÉQUENTIEL + FIXES + LOCK SYSTEM
- * Version: v1.21 | 2026-01-29
+ * Version: v1.22 | 2026-01-29
  ****************************************************/
 
 const DASHBOARD_SHEET_NAME = "Dashboard";
