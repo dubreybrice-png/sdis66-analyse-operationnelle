29a9628782318029f33efa31df07e1e3ecb3af50
SDIS 66 <sdis66@sdis66.fr>
2026-01-29 00:57:15 +0100

    v1.27: Improve error list display with better formatting for date, type, and engin

--- Code.js
+++ Code.js
@@ -1,7 +1,7 @@
 /****************************************************
  * SDIS 66 - SDS | WebApp Dashboard
  * CACHE SÉQUENTIEL + FIXES + LOCK SYSTEM
- * Version: v1.26 | 2026-01-29
+ * Version: v1.27 | 2026-01-29
  ****************************************************/
 
 const DASHBOARD_SHEET_NAME = "Dashboard";
