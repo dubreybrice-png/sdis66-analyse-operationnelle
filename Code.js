6775344ef61db17135844cd4f8a89b996e0fd0c7
SDIS 66 <sdis66@sdis66.fr>
2026-01-29 02:14:42 +0100

    Update version comments to v1.39 in all scripts

--- Code.js
+++ Code.js
@@ -1,7 +1,7 @@
 /****************************************************
  * SDIS 66 - SDS | WebApp Dashboard
  * CACHE SÉQUENTIEL + FIXES + LOCK SYSTEM + ANTI-DOUBLE-COUNT
- * Version: v1.36 | 2026-01-29
+ * Version: v1.39 | 2026-01-29
  ****************************************************/
 
 const DASHBOARD_SHEET_NAME = "Dashboard";
